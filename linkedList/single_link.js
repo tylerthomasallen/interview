3d8fa0099a321251e9ffc069b53aeb7ee212f473
Tyler <tylerthomasallen@gmail.com>
2019-02-28 12:08:35 -0800

    Remove dups from linked list

--- linkedList/single_link.js
+++ linkedList/single_link.js
@@ -11,11 +11,41 @@ class Node {
   }
 }
 
+const deleteNode = (node) => {
+  if (node.next) {
+    node.data = node.next.data;
+    node.next = node.next.next;
+  } else {
+    node.data = null;
+  }
+}
+
+const deleteDups = (rootNode) => {
+  const vals = {}
+
+  let currNode = rootNode;
+  while (currNode != null) {
+    const data = { currNode };
+    if (vals[data]) {
+      deleteNode(currNode)
+    } else {
+      vals[data] = true
+    }
+    currNode = currNode.next;
+  }
+
+  return rootNode;
+}
+
 const node1 = new Node(1);
 const node2 = new Node(2);
 const node3 = new Node(3);
+const node4 = new Node(3);
 
 node1.appendToTail(node2);
 node2.appendToTail(node3);
+node3.appendToTail(node4);
+
+deleteDups(node1);
 
 console.log(node1);
